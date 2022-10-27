c50f3f23719523fb1cf72e741439b676e1e5661c
sgraziola <simo.fdl@gmail.com>
2022-10-27 18:16:12 +0200

    SlidersVueJs: Add import/export slides array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-/* import slides from "./script.js"
+import slides from "./script.js"
 console.log(slides);
- */
+
 
 /* Descrizione:
 Partendo dal markup della versione svolta in js plain (vedi allegato), rifare lo slider ma questa volta usando Vue.
@@ -16,37 +16,7 @@ createApp ({
             activeImage : 0,
             intervalId : "",
             hover : "false",
-            carousel : [
-                    {
-                        image: './assets/img/01.webp',
-                        title: 'Marvel\'s Spiderman Miles Morale',
-                        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                    }, 
-                    
-                    {
-                        image: './assets/img/02.webp',
-                        title: 'Ratchet & Clank: Rift Apart',
-                        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                    }, 
-                    
-                    {
-                        image: './assets/img/03.webp',
-                        title: 'Fortnite',
-                        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                    }, 
-                    
-                    {
-                        image: './assets/img/04.webp',
-                        title: 'Stray',
-                        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                    }, 
-                    
-                    {
-                        image: './assets/img/05.webp',
-                        title: "Marvel's Avengers",
-                        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                    }
-                ],
+            carousel : slides,
         }
     },
 
